75521a1d81db6db6b3f9c4544ea4cce20cfb56f1
Hzmou <zmou31@gmail.com>
2025-05-19 19:49:35 -0400

    Started working on program-02

--- program-02/packageWeights.cpp
+++ program-02/packageWeights.cpp
@@ -2,3 +2,31 @@
   * This is a basic cpp program that uses control flow concepts to output information about
   * packages. 
 */
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+
+
+  double weight;
+  cout << "Enter packages weights (0 to end sequence):  ";
+  int count =0;
+
+
+while(weight !=0){
+ 
+cin >> weight;
+
+count++;
+
+}
+count = count -1;
+
+
+   
+  cout << "Number of packages: "<<count;
+
+}
