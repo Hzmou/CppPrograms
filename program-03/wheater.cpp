dc3933e99ca7084a90b41780a632abbab96f8490
Hzmou <zmou31@gmail.com>
2025-05-21 01:33:50 -0400

    Created directory for program-03 and stared the code for .cpp file

---
+++ program-03/wheater.cpp
@@ -0,0 +1,17 @@
+
+/*
+  * This is a Basic program in C++ that uses functions and file I/O to read from a file
+  * and output the weather data to the console taken from the file. 
+*/
+
+#include <iostream>
+
+
+
+
+int main(){
+
+
+
+
+}
