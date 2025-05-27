a2735d00cf683dfaadc01b7107ef76a089c1cef9
Hzmou <zmou31@gmail.com>
2025-05-27 19:51:05 -0400

    started code for program-06

--- program-06/speakers.cpp
+++ program-06/speakers.cpp
@@ -2,3 +2,33 @@
 /*
   * This is cpp program that uses structures to store data about speakers at a conference. 
 */
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+
+/* struct to represent a speakers information.
+  * It contains the speaker's name, phone number, topic, and fee.
+  * The struct is defined with public access by default.
+  */
+
+struct Speaker{
+
+  string name;
+  string phone;
+  string topic;
+  double fee;
+
+};
+
+
+
+//function prototypes.
+void display (vector<Speaker> speakers);
+int search (vector<Speaker> pool, string target);
+
+
